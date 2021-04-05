da4767b8a71eda51e9c65da6a8a7c20b046ef1cc
07test9992 <81176013+07test9992@users.noreply.github.com>
2021-04-05 15:34:01 -0500

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -24,7 +24,7 @@ module.exports = class PingCommand extends Command {
                     "text": `Join our support server! https://discord.gg/${process.env.invite}`
                 },
                 "author": {
-                    "name": "Shortlink Bot",
+                    "name": "LinkV Bypass",
                     "url": "https://github.com/07test9992/test",
                     "icon_url": "https://cdn.discordapp.com/avatars/780857188171644962/0344f614c6e85bef212f77d24631c631.webp?size=128"
                 }
