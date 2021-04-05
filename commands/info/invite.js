7ad1707718ab9109db8c97409122ddcce26bdd6f
07test9992 <81176013+07test9992@users.noreply.github.com>
2021-04-05 15:32:31 -0500

    Update invite.js

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -15,8 +15,8 @@ module.exports = class InviteCommand extends Command {
             "title": "Invite",
             "description": "[Click me!](https://discord.com/oauth2/authorize?client_id=820713791884361768&scope=bot&permissions=8)",
             "author": {
-                "name": "Shortlink Bot",
-                "url": "https://github.com/Rismose/shortlink-bot",
+                "name": "LinkV Bypass",
+                "url": "https://github.com/07test9992/test",
                 "icon_url": "https://cdn.discordapp.com/avatars/780857188171644962/0344f614c6e85bef212f77d24631c631.webp?size=128"
             }
         });
