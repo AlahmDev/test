cb371c9304c988ba487411ab853be9dc04683957
07test9992 <81176013+07test9992@users.noreply.github.com>
2021-04-05 10:37:24 -0500

    Update info.js

--- commands/info/info.js
+++ commands/info/info.js
@@ -14,7 +14,7 @@ module.exports = class InfoCommand extends Command {
         return message.embed({
             "title": "Information",
             "fields": [{
-                "name": "Hi! I'm Shortlink Bot.",
+                "name": "Hi! I'm LinkV Bypass.",
                 "value": ":wave:"
             }, {
                 "name": "I find what's behind the shortlink, like adf.ly, bit.ly and Linkvertise!",
@@ -24,11 +24,11 @@ module.exports = class InfoCommand extends Command {
                 "value": "the help command!"
             }, {
                 "name": "Bypasses:",
-                "value": "Bypasses by lemons#7106 / respecting <3"
+                "value": "Bypasses by 07test#9992"
             }],
             "author": {
-                "name": "Shortlink Bot",
-                "url": "https://github.com/Rismose/shortlink-bot",
+                "name": "LinkV Bypass",
+                "url": "https://github.com/07test9992/test",
                 "icon_url": "https://cdn.discordapp.com/avatars/780857188171644962/0344f614c6e85bef212f77d24631c631.webp?size=128"
             }
         });
