ae0ac5f719fcfac3ee5d39a96c45f2975e7fa3e8
07test9992 <81176013+07test9992@users.noreply.github.com>
2021-04-15 09:20:26 -0500

    Added a vote command

--- commands/info/vote.js
+++ commands/info/vote.js
@@ -1,1 +1,24 @@
+const {Command} = require('discord.js-commando');
 
+module.exports = class InviteCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'invite',
+            group: 'info',
+            memberName: 'invite',
+            description: 'Replies with the bots vote link.',
+        });
+    }
+
+    run(message) {
+        return message.embed({
+            "title": "Vote",
+            "description": "[Click me!](Your Bots vote link.)",
+            "author": {
+                "name": "LinkV Bypass",
+                "url": "https://github.com/07test9992/test",
+                "icon_url": "https://cdn.discordapp.com/avatars/780857188171644962/0344f614c6e85bef212f77d24631c631.webp?size=128"
+            }
+        });
+    }
+};
